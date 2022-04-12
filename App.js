5edd86999e58db1d82b3e12c8dd8c9c80092a187
Roy <rott.9954@gmail.com>
2022-04-11 19:35:52 -0500

    chore: added new dependency

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Amplify from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react-native";
-import { StatusBar } from 'expo-status-bar';
 import MainNavigator from './src/Navigator';
 import { GlobalProvider } from './src/context/global/global.context';
 import awsconfig from "./src/aws-exports";
+import { LogBox } from 'react-native';
+
+;
 
 Amplify.configure(awsconfig);
 
 function App({authData}) {
   //console.log(authData)
+  LogBox.ignoreLogs(['Setting a timer'])
   return (
     <GlobalProvider authData={authData}>
       <MainNavigator />
