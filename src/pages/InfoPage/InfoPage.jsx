a5fc82827ccb800660b4c597a2e0a7ee8ab49f91
Roy <rott.9954@gmail.com>
2022-04-12 22:05:32 -0500

    fix: userinfo

--- src/pages/InfoPage/InfoPage.jsx
+++ src/pages/InfoPage/InfoPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
+import { Avatar } from "react-native-paper";
 import { styles } from "./Info.styles";
 import { Auth } from "aws-amplify";
 import i18n from "../../localization/i18n";
 
-export default function InfoPage({onPress, state}) {
+export default function InfoPage({ onPress, state }) {
   const logout = async () => {
     try {
       await Auth.signOut();
@@ -14,12 +15,13 @@ export default function InfoPage({onPress, state}) {
   };
   return (
     <View style={styles.container}>
-       <Avatar.Image 
-    rounded
-    style={styles.iconUser}
-    source={require('../../../assets/user-ico.png')} />
+      <Avatar.Image
+        rounded
+        style={styles.iconUser}
+        source={require("../../../Media/imagenes/user-ico.png")}
+      />
       <Text>{state.attributes.email}</Text>
-      <View style={{ margin: 12, width: 200}}>
+      <View style={{ margin: 12, width: 200 }}>
         <Button title={i18n.t("logout")} onPress={logout}></Button>
       </View>
     </View>
