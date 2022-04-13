3ea39ec6edd0849c1d54f8882870a6a7b9350d2c
karla16Santoyo99 <64182574+karla16Santoyo99@users.noreply.github.com>
2022-04-12 20:50:42 -0500

    prueba

--- src/pages/HomePage/HomePage.styles.js
+++ src/pages/HomePage/HomePage.styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
+        backgroundColor: "#D3F2F9",
         alignItems: "center",
         justifyContent: "center"
     },
